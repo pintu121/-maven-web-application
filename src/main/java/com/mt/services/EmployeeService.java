46aa92140dd50ab240b77b27e2d4db983c7d9e08
Pintu Patra <46493309+pintu121@users.noreply.github.com>
2022-12-13 20:11:27 +0530

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,9 +22,9 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
+		js.put("Name", "Pintu Patra");
+		js.put("Calling Name", "Pintu");
+		js.put("DOB", "01-May-1997");
 		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
 		js.put("Places he like", "His native place");
 
